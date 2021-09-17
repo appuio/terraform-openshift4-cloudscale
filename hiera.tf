89a199c0f9178800776cab2b7891a0d66d7dba41
Sebastian Widmer <sebastian.widmer@vshn.net>
2021-09-17 15:24:44 +0200

    More generic hiera module

--- hiera.tf
+++ hiera.tf
@@ -1,15 +1,16 @@
 module "hiera" {
   source = "git::https://github.com/appuio/terraform-modules.git//modules/lb-vshn-hiera?ref=skeleton-and-lb"
 
-  router_ip_addresses      = module.infra.ip_addresses[*]
+  api_backends             = formatlist("etcd-%d.${local.node_name_suffix}", range(3))
+  router_backends          = module.infra.ip_addresses[*]
   bootstrap_node           = var.bootstrap_count > 0 ? cidrhost(var.privnet_cidr, 10) : ""
   node_name_suffix         = local.node_name_suffix
   cluster_id               = var.cluster_id
-  privnet_cidr             = var.privnet_cidr
   lb_names                 = module.lb.server_names
   lb_cloudscale_api_secret = var.lb_cloudscale_api_secret
   hieradata_repo_user      = var.hieradata_repo_user
-  api_vip_network          = module.lb.api_vip[0].network
-  nat_vip_network          = module.lb.nat_vip[0].network
-  router_vip_network       = module.lb.router_vip[0].network
+  api_vip                  = cidrhost(module.lb.api_vip[0].network, 0)
+  internal_vip             = cidrhost(var.privnet_cidr, 100)
+  nat_vip                  = cidrhost(module.lb.nat_vip[0].network, 0)
+  router_vip               = cidrhost(module.lb.router_vip[0].network, 0)
 }
