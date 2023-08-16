d154df1fcf04c5e5a5b5741ba982dd8d33a7b2e5
Simon Gerber <simon.gerber@vshn.ch>
2023-08-16 13:57:48 +0200

    fix formatting

--- modules/cloudscale-lb/providers.tf
+++ modules/cloudscale-lb/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.3.0"
   required_providers {
     cloudscale = {
-      source = "cloudscale-ch/cloudscale"
+      source  = "cloudscale-ch/cloudscale"
       version = "4.2.0"
     }
   }
