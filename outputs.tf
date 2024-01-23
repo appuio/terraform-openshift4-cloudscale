1d7021ce63625fb78884ff1bc0f404d10c54a821
Simon Gerber <simon.gerber@vshn.ch>
2024-01-23 14:25:23 +0100

    Remove unnecessary bits for no-puppet setup

--- outputs.tf
+++ outputs.tf
@@ -14,13 +14,10 @@ output "dns_entries" {
     "api_vip_v6"          = cloudscale_floating_ip.api_v6.id
     "router_vip"          = local.router_vip
     "router_vip_v6"       = local.router_vip_v6
-    "egress_vip"          = var.enable_nat_vip && var.lb_count != 0 ? split("/", module.lb.nat_vip[0].network)[0] : ""
     "internal_vip"        = local.internal_vip,
     "internal_router_vip" = var.internal_router_vip,
     "masters"             = module.master.ip_addresses,
     "cluster_id"          = var.cluster_id,
-    "lbs"                 = module.lb.public_ipv4_addresses,
-    "lb_hostnames"        = module.lb.server_names
   })
 }
 
@@ -50,10 +47,6 @@ output "ignition_ca" {
 
 output "api_int" {
   value = "api-int.${local.node_name_suffix}"
-}
-
-output "hieradata_mr" {
-  value = module.lb.hieradata_mr_url
 }
 
 output "master-machines_yml" {
